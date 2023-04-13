485da05cda29fa2c36443e5025372e37aa479104
nmndeep <nmndeep@gmail.com>
2023-04-13 09:48:53 +0200

    cleaned up

--- semseg/datasets/__init__.py
+++ semseg/datasets/__init__.py
@@ -1,45 +1,28 @@
-from .ade20k import ADE20K
+from .ade import ADE20KSegmentation
 from .camvid import CamVid
 from .cityscapes import CityScapes
 from .pascalcontext import PASCALContext
 from .cocostuff import COCOStuff
-from .sunrgbd import SunRGBD
-from .mapillary import MapillaryVistas
-from .mhpv1 import MHPv1
-from .mhpv2 import MHPv2
-from .lip import LIP
 from .cihp import CIHP, CCIHP
-from .atr import ATR
 from .suim import SUIM
-from .helen import HELEN
-from .lapa import LaPa
-from .ibugmask import iBugMask
 from .celebamaskhq import CelebAMaskHQ
-from .facesynthetics import FaceSynthetics
-# from .builder import build_dataloader
-# from .builder import build_dataset
-# from .distributed_sampler import DistributedSampler
+from .distributed_sampler import DistributedSampler, IterationBasedBatchSampler
+from .ade import get_segmentation_dataset, make_data_sampler, make_batch_data_sampler
 
 __all__ = [
     'CamVid',
     'CityScapes',
-    'ADE20K',
-    'MHPv1',
-    'MHPv2',
-    'LIP',
+    'ADE20KSegmentation',
     'CIHP',
     'CCIHP',
-    'ATR',
     'PASCALContext',
     'COCOStuff',
     'SUIM',
-    'SunRGBD',
-    'MapillaryVistas',
-    'HELEN',
-    'LaPa',
-    'iBugMask',
     'CelebAMaskHQ',
-    'FaceSynthetics',
-    # 'build_dataloader',
-
+    'IterationBasedBatchSampler',
+    'DistributedSampler',
+    'get_segmentation_dataset',
+    'make_data_sampler', 
+    'make_batch_data_sampler'
 ]
+
--- semseg/datasets/ade.py
+++ semseg/datasets/ade.py
@@ -2,9 +2,15 @@
 import os
 import torch
 import numpy as np
+import torch.utils.data as data
+
+from torchvision import transforms
+
+from torch import distributed as dist
 
 from PIL import Image
 from .dataset_wrappers import SegmentationDataset
+from .distributed_sampler import DistributedSampler, IterationBasedBatchSampler
 
 
 class ADE20KSegmentation(SegmentationDataset):
@@ -166,6 +172,30 @@ def _get_ade20k_pairs(folder, mode='train'):
                 print('cannot find the mask:', maskpath)
 
     return img_paths, mask_paths
+
+datasets= {'ade20k': ADE20KSegmentation}
+
+
+def get_segmentation_dataset(name, **kwargs):
+    """Segmentation Datasets"""
+    return datasets[name.lower()](**kwargs)
+
+
+def make_data_sampler(dataset, shuffle, distributed=True):
+    if distributed:
+        return DistributedSampler(dataset, shuffle=shuffle)
+    if shuffle:
+        sampler = RandomSampler(dataset)
+    else:
+        sampler = SequentialSampler(dataset)
+    return sampler
+
+def make_batch_data_sampler(sampler, images_per_batch, num_iters=None, start_iter=0):
+    batch_sampler = data.sampler.BatchSampler(sampler, images_per_batch, drop_last=True)
+    if num_iters is not None:
+        batch_sampler = IterationBasedBatchSampler(batch_sampler, num_iters, start_iter)
+    return batch_sampler
+
 
 
 if __name__ == '__main__':
