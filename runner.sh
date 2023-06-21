d94583bd61b77016706b9413765fa3730d1327e3
nmndeep <nmndeep@gmail.com>
2023-06-21 10:45:41 +0200

    clean up, remove redundant files

--- runner.sh
+++ runner.sh
@@ -2,10 +2,10 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4  
 #SBATCH --time=20:00:00
-#SBATCH --gres=gpu:8
-#SBATCH --job-name=ADE_CVXT_T
-#SBATCH --output=../JobLogs/ADE_CVXT_T_%j.out
-#SBATCH --error=../JobLogs/ADE_CVXT_T_%j.err
+#SBATCH --gres=gpu:4
+#SBATCH --job-name=ROB_SEG
+#SBATCH --output=../JobLogs/ROB_SEG_%j.out
+#SBATCH --error=../JobLogs/ROB_SEG_%j.err
 # print info about current job
 
 scontrol show job $SLURM_JOB_ID 
--- runner_infer.sh
+++ runner_infer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=2  
-#SBATCH --time=6:00:00
+#SBATCH --time=5:00:00
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=ADE_CVXT_T
 #SBATCH --output=../JobLogs/ADE_CVXT_T_%j.out
@@ -12,4 +12,4 @@ scontrol show job $SLURM_JOB_ID
 #conda activate main_py
 		
 
-python3 ./tools/infer.py --adversarial --pair $1 --eps $2 #--world_size $2
+python3 ./tools/infer.py --adversarial --eps $2 #--world_size $2
