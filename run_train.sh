313c6d455b61539eb256d9084359e3a910ce0eb6
nmndeep <nmndeep@gmail.com>
2023-07-04 22:20:32 +0200

    Delete run_train.sh

--- run_train.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-# rn50_configs/rn50_16_epochs.yaml
-# #--config-file $1 \
-sleep 2s
-
-python3 ./tools/train.py --cfg ./configs/ade20k_convnext_cvst.yaml --world_size 8
